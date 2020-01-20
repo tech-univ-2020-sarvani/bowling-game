4b1cc1ad53cbc78f7d166621d6de8738b41f7498
sarvanideekshitula <sarvanimini@gmail.com>
2020-01-20 12:08:45 +0530

    Computing score of the bowlingGame using Javascript

---
+++ bowlingGame.js
@@ -0,0 +1,83 @@
+let throwCount = 0;
+FrameScores = [];
+
+// const frame = ((first=0, second = 0) => {
+//     const obj = {};
+//     this.firstRoll = first;
+//     this.secondRoll = second;
+//     this.frameScore = this.firstRoll+this.secondRoll;
+// });
+
+Frames = [];
+
+const bowlingGameScore = ((scores) => {
+    let frameLength = 20;
+    for(let i=0;i<frameLength-1; i++){
+        if(scores[i] == 10){
+            Frames.push(scores[i]+ scores[i+1]+ scores[i+2]);
+        }
+        else if(scores[i]+scores[i+1] == 10){
+            Frames.push(scores[i]+scores[i+1]+scores[i+2]);
+            i = i+1;
+        }
+        else{
+            Frames.push(scores[i]+scores[i+1]);
+            i = i+1;
+        }
+    }
+    // scores.forEach((throwScore) => {
+    //     roll(throwScore);
+    // });
+    const result = score(Frames);
+    return result;
+});
+
+// let spareFlag = 0;
+
+// const roll = ((throwScore) => {
+//     throwCount++;
+//     if(throwCount%2 != 0 && throwScore == 10){
+//         throwCount=0;
+//         strike();
+//     }
+//     else if(throwCount%2 == 0 && throwScore == 10){
+//         if(spareFlag == 0){
+//             spare();
+//             spareFlag = 1;
+//         }
+//         else{
+//             spareFlag = 0;
+//         }
+//     }
+//     else{
+//         if(spareFlag == 0){
+//             addScore();
+//             spareFlag = 1;
+//         }
+//         else{
+//             spareFlag = 0;
+//         }
+//     }
+// });
+// const strike = (() => {
+//     totalScore = totalScore + Frames[throwCount];
+// });
+
+// const  spare = (() => {
+//     totalScore = totalScore + Frames[throwCount/2];
+// });
+// const addScore = (() => {
+//     totalScore = totalScore + Frames[throwCount/2]
+// });
+
+const score = ((Frames) => {
+    let totalScore = 0;
+    for(let i=0;i<Frames.length;i++){
+        totalScore = totalScore+Frames[i];
+    }
+    return totalScore;
+});
+
+module.exports = bowlingGameScore;
+module.exports = score;
+// console.log(bowlingGameScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
--- bowlingGame.test.js
+++ bowlingGame.test.js
@@ -3,35 +3,35 @@ const roll = require('./bowlingGame');
 const score = require('./bowlingGame');
 
 describe('The function bowlingGameScore', () => {
-    it('should returns the value returned by the function score', () => {
+    it('should returns the totalScore calculated by the function score', () => {
         const scores = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
         const result = bowlingGameScore(scores);
         expect(result).toEqual(90);
     });
 });
-describe('The function roll', () => {
-    it('should call function strike, if the no.of throws is odd and no.of pins knocked down is 10', () => {
-        strike = jest.fn();
-        roll(10);
-        expect(strike).toHaveBeenCalled();
-    });
-    it('should call function spare, if the no.of throws is even and no.of pins knocked down in the frame is summed to 10', () => {
-        spare = jest.fn();
-        roll(5);
-        roll(5);
-        expect(spare).toHaveBeenCalled();
-    });
-    it('should call function addScore, if the no.of throws is even and no.of pins knocked down in the frame is not summed to 10', () => {
-        addScore = jest.fn();
-        roll(6);
-        roll(3);
-        expect(addScore).toHaveBeenCalled();
-    });
-});
+// describe('The function roll', () => {
+//     it('should call function strike, if the no.of throws is odd and no.of pins knocked down is 10', () => {
+//         strike = jest.fn();
+//         roll(10);
+//         expect(strike).toHaveBeenCalled();
+//     });
+//     it('should call function spare, if the no.of throws is even and no.of pins knocked down in the frame is summed to 10', () => {
+//         spare = jest.fn();
+//         roll(5);
+//         roll(5);
+//         expect(spare).toHaveBeenCalled();
+//     });
+//     it('should call function addScore, if the no.of throws is even and no.of pins knocked down in the frame is not summed to 10', () => {
+//         addScore = jest.fn();
+//         roll(6);
+//         roll(3);
+//         expect(addScore).toHaveBeenCalled();
+//     });
+// });
 
 describe('The function score', () => {
     it('should return a number', () => {
-        const result = score();
+        const result = score(10);
         expect(typeof result).toBe('number');
     });
 });
